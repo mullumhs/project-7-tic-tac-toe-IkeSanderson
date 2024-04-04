fcb43597a30a10e70fc05da1b364f15087e18ba3
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-04-04 13:09:25 +1100

    Started connect 4

--- connect-four.py
+++ connect-four.py
@@ -1,26 +1,42 @@
-def main():
-    board = initialiseBoard()
-    displayBoard(board)
-    CurrentPlayer = 1
-    
 def initialiseBoard():
     board = []
-    for _ in range(6):
+    for rows in range(6):
         row = []
-        for _ in range(7):
-            row.append('[]')
+        for col in range(7):
+            row.append('_')
         board.append(row)
     return board
+         
     
 def displayBoard(board):
+    print("_1_2_3_4_5_6_7_")
     for row in board:
+        print("", end = "|")
         for cell in row:
-            print(cell, end=' ')
+            print(cell, end = "|")
         print()
-    print()
 
-def TakeTurn():
-    print()
+def takeTurn(turnCount, board):
+    
+    if turnCount % 2 == 0:
+        token = 'O'
+        turn = int(input("Player 2 Select a Collum (1 - 7):"))
+       
+    else:
+        token = 'X'
+        turn = int(input("Player 1 Select a Collum (1 - 7):"))
+    turn -= 1
+    for Row in range(5,-1,-1):
+       if board[Row][turn] == '_':
+        board[Row][turn] = token
+        turnCount += 1
+        return turnCount
 
-if __name__ == "__main__":
-    main()
+def main(): 
+    turnCount = 1
+    board = initialiseBoard()
+    while True:        
+        displayBoard(board)
+        turnCount = takeTurn(turnCount, board)
+
+main()
