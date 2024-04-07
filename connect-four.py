e742962929822a39a4f312e89fb8b2370211e543
IkeSanderson <140124177+IkeSanderson@users.noreply.github.com>
2024-04-08 09:49:55 +1000

    Finished diagonal win check

--- connect-four.py
+++ connect-four.py
@@ -27,11 +27,14 @@ def takeTurn(turnCount, board):
                 token = 'X'
                 turn = int(input("Player 1 Select a Collum (1 - 7):"))
             turn -= 1
-            for Row in range(5,-1,-1):
-                if board[Row][turn] == '_':
-                    board[Row][turn] = token
-                    turnCount += 1
-                    return turnCount
+            if not turn > 0 and turn < 7:
+                for Row in range(5,-1,-1):
+                    if board[Row][turn] == '_':
+                        board[Row][turn] = token
+                        turnCount += 1
+                        return turnCount
+            else:
+                print("Please Enter a Valid Move")
         except:
             print("Please Enter a Valid Move")
 
@@ -42,25 +45,32 @@ def checkDraw(board):
                 return False
     return True
 
-def checkHorizontal(board, Winner):
+def checkHorizontal(board):
     for row in range(6):
         for col in range(4):
             if (board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] != '_') :
-                Winner == 'X'
-                return Winner
-            elif (board[row][col] == board[row][col + 1] == board[row][col + 2] == board[row][col + 3] != '_') :
-                Winner == 'O'
-                return Winner
+                return  True
+           
             
-def checkVertical(board, Winner):
+def checkVertical(board):
     for col in range(7):
         for row in range(3):
             if (board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] != '_' ) :
-                Winner == 'X'
-                return Winner
-            elif (board[row][col] == board[row + 1][col] == board[row + 2][col] == board[row + 3][col] != '_' ) :
-                Winner == 'O'
-                return Winner
+                return True
+           
+            
+def checkDiagonal(board):
+    for row in range(3):
+        for col in range(4):
+            if (board[row][col] == board[row + 1][col + 1] == board[row + 2][col + 2] == board[row + 3][col + 3] != '_' ) :
+              return True
+            elif (board[row][col] == board[row + 1][col - 1] == board[row + 2][col - 2] == board[row + 3][col - 3] != '_' ) :
+              return True
+            elif (board[row][col] == board[row - 1][col + 1] == board[row - 2][col + 2] == board[row - 3][col + 3] != '_' ) :
+              return True
+            elif (board[row][col] == board[row - 1][col - 1] == board[row - 2][col - 2] == board[row - 3][col - 3] != '_' ) :
+              return True
+           
 
 
 def main(): 
@@ -72,18 +82,37 @@ def main():
         displayBoard(board)
         turnCount = takeTurn(turnCount, board)
        
-        if checkHorizontal(board, Winner) == True:
+        if checkHorizontal(board) == True:
             displayBoard(board)
-            print(f"{Winner} Win!")
+            if turnCount % 2 == 0:
+                print(" X Win!")
+            else:
+                print(" O Win!")
             break
         
-        if checkVertical(board, Winner) == True:
-            displayBoard(board)
-            print(f"{Winner} Win!")
             break
-
+       
+        if checkVertical(board) == True:
+            displayBoard(board)
+            if turnCount % 2 == 0:
+                print(" X Win!")
+            else:
+                print(" O Win!")
+            break
+        
+        if checkDiagonal(board) == True:
+            displayBoard(board)
+            if turnCount % 2 == 0:
+                print(" X Win!")
+            else:
+                print(" O Win!")
+            break
+        
         if checkDraw(board) == True:
             print("Draw!")
             break
-
-main()
+while True:
+    print()
+    print("-Welcome To Connect 4 -")
+    print()
+    main()
